f85a798531e64ceeb5045be1dec21456567d3944
Samantha Baneth <sambaneth8@gmail.com>
2022-11-04 21:44:29 -0600

    completed win conditions

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ class Game:
         self.player_two = AI(2) # None
     
     def rules(self):
-        print('\nScissors decapitate Scissors cuts paper, paper covers rock, rock crushes lizard, lizard poisons Spock, Spock smashes scissors, scissors decapitates lizard, lizard eats paper, paper disproves Spock, Spock vaporizes rock, and as it always has, rock crushes scissors.\n')
+        print('\nScissors cuts paper, paper covers rock, rock crushes lizard, lizard poisons Spock, Spock smashes scissors, scissors decapitates lizard, lizard eats paper, paper disproves Spock, Spock vaporizes rock, and as it always has, rock crushes scissors.\n')
     
     def choose_game_mode(self):
         pass
@@ -18,7 +18,7 @@ class Game:
             self.player_two.choose_gesture()
             if self.player_one.gesture == self.player_two.gesture:
                 print("its a tie, keep playing")
-            if (self.player_one.gesture == "rock" and self.player_two.gesture == "scissors") or (self.player_one.gesture == "rock" and self.player_two.gesture == "lizard") or (self.player_one.gesture == "lizard" and self.player_two.gesture == "spock"):
+            if (self.player_one.gesture == "rock" and self.player_two.gesture == "scissors") or (self.player_one.gesture == "rock" and self.player_two.gesture == "lizard") or (self.player_one.gesture == "lizard" and self.player_two.gesture == "spock") or (self.player_one.gesture == "scissors" and self.player_two.gesture == "paper") or (self.player_one.gesture == "paper" and self.player_two.gesture == "rock") or (self.player_one.gesture == "lizard" and self.player_two.gesture == "spock") or (self.player_one.gesture == "spock" and self.player_two.gesture == "scissors") or (self.player_one.gesture == "scissors" and self.player_two.gesture == "lizard") or (self.player_one.gesture == "lizard" and self.player_two.gesture == "paper") or (self.player_one.gesture == "paper" and self.player_two.gesture == "spock") or (self.player_one.gesture == "spock" and self.player_two.gesture == "rock"):
                 self.player_two.score -= 1
 
     def winner(self):   
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from game import Game
 
 game = Game()
-game.rules
+game.rules()
 game.choose_game_mode()
 game.game()
 game.winner()
